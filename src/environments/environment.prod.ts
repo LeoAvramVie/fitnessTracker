c4b7643bb2a26be7a28002becf615d01b9264baa
Leo Avramovic <ziva.avramovic@twinformatics.at>
2020-08-29 19:58:12 +0200

    feat: first build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDgf9yB9dWimfdd7sUtVi-rbKTKDCxIVqE',
+    authDomain: 'ng-fitness-tracker-8e468.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-8e468.firebaseio.com',
+    projectId: 'ng-fitness-tracker-8e468',
+    storageBucket: 'ng-fitness-tracker-8e468.appspot.com',
+    messagingSenderId: '911304582244',
+    appId: '1:911304582244:web:6b492b28c07de3fa2f2b71',
+    measurementId: 'G-FMMZQMKK14'
+  }
 };
