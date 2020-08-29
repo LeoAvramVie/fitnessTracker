a941fc76aa1ad74c51ea85e9cda597ee6426c226
Leo Avramovic <ziva.avramovic@twinformatics.at>
2020-08-29 19:54:38 +0200

    fix: finished the lazyloading stuff and mov the auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,6 @@ import {AngularFirestoreModule} from 'angularfire2/firestore';
 import {environment} from '../environments/environment';
 import {UiServiceService} from './shared/ui.service.service';
 import {AuthModule} from './auth/auth.module';
-import {TrainingModule} from './training/training.module';
 
 @NgModule({
   declarations: [
@@ -37,7 +36,7 @@ import {TrainingModule} from './training/training.module';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AuthModule,
-    TrainingModule
+
   ],
   providers: [AuthService, TrainingService, UiServiceService],
   bootstrap: [AppComponent],
--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -3,11 +3,10 @@ import {TrainingComponent} from './training.component';
 import {CurrentTrainingComponent} from './current-training/current-training.component';
 import {NewTrainingComponent} from './new-training/new-training.component';
 import {PastTrainingComponent} from './past-training/past-training.component';
-import {MaterialModule} from '../material.module';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {CommonModule} from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {StopTrainingComponent} from './current-training/stop-training.component';
+import {SharedModule} from '../shared/shared.module';
+import {TrainingRouterModule} from './training-router.module';
+
 
 @NgModule({
   declarations: [
@@ -15,14 +14,12 @@ import {StopTrainingComponent} from './current-training/stop-training.component'
     CurrentTrainingComponent,
     NewTrainingComponent,
     PastTrainingComponent,
-    StopTrainingComponent
+    StopTrainingComponent,
+
   ],
   imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MaterialModule,
-    FlexLayoutModule,
+    SharedModule,
+    TrainingRouterModule
   ],
   exports: []
 })
