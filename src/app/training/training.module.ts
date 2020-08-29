6701ce794293468998a057476b2f62fe10103051
Leo Avramovic <ziva.avramovic@twinformatics.at>
2020-08-29 18:41:57 +0200

    fix: arrange feature modules

---
+++ src/app/training/training.module.ts
@@ -0,0 +1,31 @@
+import {NgModule} from '@angular/core';
+import {TrainingComponent} from '../training.component';
+import {CurrentTrainingComponent} from './current-training.component';
+import {NewTrainingComponent} from '../new-training/new-training.component';
+import {PastTrainingComponent} from '../past-training/past-training.component';
+import {MaterialModule} from '../../material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {StopTrainingComponent} from './stop-training.component';
+
+@NgModule({
+  declarations: [
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent,
+    StopTrainingComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule,
+  ],
+  exports: []
+})
+export class TrainingModule {
+
+}
